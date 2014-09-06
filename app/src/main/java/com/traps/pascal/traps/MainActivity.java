01e2b1c6da10344b2879dbf3e3a65b5eae315b83
PascalFriko <pascal.friko@googlemail.com>
2014-09-06 20:55:47 +0200

    moved "GetLocationTask.java" in "MainActivity.java" and improved location accuracy

--- app/src/main/java/com/traps/pascal/traps/MainActivity.java
+++ app/src/main/java/com/traps/pascal/traps/MainActivity.java
@@ -2,17 +2,24 @@ package com.traps.pascal.traps;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.Context;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
 import android.location.LocationManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.location.LocationListener;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+
+import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends Activity {
@@ -26,8 +33,10 @@ public class MainActivity extends Activity {
         if (checkGooglePlayServices()) {
             Log.d("Google Play Services", "Success");
 
-            GetLocationTask locTask = new GetLocationTask(this);
-            locTask.execute();
+            Location startLocation = getStartLocation();
+            Log.d("StartLcoation", startLocation.toString());
+
+
         } else {
             //What to do??
         }
@@ -73,5 +82,82 @@ public class MainActivity extends Activity {
         return false;
     }
 
+    private Location getStartLocation() {
+        Location location = null;
+        double longitude = 0.0;
+        double latitude = 0.0;
+        double altitude = 0.0;
+        String provider;
+        String address;
+        Geocoder geocoder;
 
+        LocationManager locationManager = (LocationManager) this.getSystemService(this.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        criteria.setCostAllowed(false);
+        criteria.setPowerRequirement(Criteria.NO_REQUIREMENT);
+        provider = locationManager.getBestProvider(criteria, false);
+
+        LocationListener myLocationListener = new MyLocationListener();
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            provider = LocationManager.GPS_PROVIDER;
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,myLocationListener);
+        } else if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            provider = LocationManager.NETWORK_PROVIDER;
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,myLocationListener);
+        } else {
+            provider = LocationManager.PASSIVE_PROVIDER;
+            locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER,0,0,myLocationListener);
+        }
+
+        location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+            altitude = location.getAltitude();
+            Log.d("Longitude", ""+longitude);
+            Log.d("Latitude", ""+latitude);
+            Log.d("Altitude", ""+altitude);
+        } else {
+            Log.d("Location Error", "Location == null");
+        }
+
+        List<Address> addressList = null;
+        geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            addressList = geocoder.getFromLocation(latitude,longitude,1);
+            Log.d("AddressList", addressList.toString());
+            String addres = addressList.get(0).getAddressLine(0);
+            String city = addressList.get(0).getAddressLine(1);
+            String zip_code = addressList.get(0).getPostalCode();
+            String country = addressList.get(0).getCountryName();
+            address = Html.fromHtml(addres +", "+ city+", "+zip_code+", "+country).toString();
+            Log.d("Address", address);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d("Error Address", "Address couldn't be fetched");
+        }
+
+        return location;
+
+    }
+
+    private class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            location.getLatitude();
+            location.getLongitude();
+            location.getAltitude();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+    }
 }
